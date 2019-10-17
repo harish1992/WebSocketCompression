ca57d000c15c537fd2d91c9f244c0468fc557bad
Harish Kumar S <hakumars@in.ibm.com>
2019-10-17 12:41:13 +0530

    prototype

--- Package.swift
+++ Package.swift
@@ -15,8 +15,6 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", .branch("master")),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,7 +28,7 @@ let package = Package(
         ),
         .target(
             name: "WebSocketCompression",
-            dependencies: ["NIO", "NIOFoundationCompat", "NIOHTTP1", "NIOSSL", "NIOWebSocket", "NIOConcurrencyHelpers", "NIOExtras", "CZlib"]),
+            dependencies: ["NIO", "NIOFoundationCompat", "NIOHTTP1", "NIOWebSocket", "CZlib"]),
         .testTarget(
             name: "WebSocketCompressionTests",
             dependencies: ["WebSocketCompression"]),
